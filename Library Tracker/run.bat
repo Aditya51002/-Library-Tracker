d9933b49260e31bca33d31737a5a7050ec6ce48b
Aditya <adityamaurya510@gmail.com>
2025-07-23 22:41:56 +0530

    Delete Library Tracker directory

--- Library Tracker/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Library Management System...
-echo.
-cd /d "C:\Users\ADITYA MAURYA\OneDrive\Desktop\libmangemt"
-java -cp ".;lib/mysql-connector-j-9.3.0.jar;lib/rs2xml.jar" Main
-pause
